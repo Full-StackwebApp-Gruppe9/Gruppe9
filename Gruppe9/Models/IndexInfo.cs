fa77d2388fc29f71e6a1d739215cb29e6c4cd5bb
Full-StackwebApp-Gruppe9 <261660@usn.no>
2025-05-20 15:54:00 +0200

    Update IndexInfo.cs

---
+++ Gruppe9/Models/IndexInfo.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gruppe9.Models;
+
+public class IndexInfo
+{
+    public int ID { get; set; } //Primary Key
+    [Required]
+    public string? Code { get; set; }
+    [Required]
+    public string? DisplayName { get; set; }
+
+    public int Value { get; set; }
+
+    public string? Category { get; set; }
+    [Required]
+
+    public string? IndexDescription { get; set; }
+    [Required]
+    public int ColorInfoId { get; set; }
+}
