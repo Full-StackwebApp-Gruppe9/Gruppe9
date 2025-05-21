5ae524409acf0d96f7b5bb0633286f8c23bf23a1
OrpRezzem <157352278+OrpRezzem@users.noreply.github.com>
2025-05-21 09:27:10 +0200

    Oppgave 2.1 Opprettet PolleAPIController ocjh vinsning är färdigt

---
+++ Gruppe9/Controllers/PollenAPIController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gruppe9.Controllers
+{
+    public class PollenAPIController : Controller
+    {
+        // GET: PollenAPIController
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
